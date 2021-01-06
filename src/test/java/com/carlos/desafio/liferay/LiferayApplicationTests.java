a59f0f349fc5d1d054d51bcc5b3e43ac0562d18b
carlosnsilva <cmanoel17@gmail.com>
2021-01-05 21:17:39 -0300

    first commit

---
+++ src/test/java/com/carlos/desafio/liferay/LiferayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.desafio.liferay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiferayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
